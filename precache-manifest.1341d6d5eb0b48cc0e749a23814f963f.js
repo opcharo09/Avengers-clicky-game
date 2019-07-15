358fb1876b32c45c7722c1e338c55119bcdfe588
unknown <oscar.charo09@gmail.com>
2019-07-14 20:42:13 -0500

    Updates

---
+++ precache-manifest.1341d6d5eb0b48cc0e749a23814f963f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39d1c839da856f714e10b284f21041c5",
+    "url": "/Avengers-clicky-game/index.html"
+  },
+  {
+    "revision": "65534f0ac83021c86227",
+    "url": "/Avengers-clicky-game/static/css/main.65f79b0a.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/Avengers-clicky-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "65534f0ac83021c86227",
+    "url": "/Avengers-clicky-game/static/js/main.29b0efda.chunk.js"
+  },
+  {
+    "revision": "2bfff7f478a2a9433166",
+    "url": "/Avengers-clicky-game/static/js/runtime~main.5b50e82f.js"
+  }
+]);
